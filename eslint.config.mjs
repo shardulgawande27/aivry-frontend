fd8e56568cd1ff30d3c3fbc626e53f75fd0719b5
shardulgawande27 <shardulgawande27@gmail.com>
2025-02-20 10:54:07 +0530

    eslint and nextconfig changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,9 +12,11 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    extends: "next/core-web-vitals",
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "@next/next/no-img-element": "off",
     },
   },
 ];
